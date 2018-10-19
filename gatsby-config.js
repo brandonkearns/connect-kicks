306f452959ea2f2298fc30b4d4a92c89c318500e
Brandon Kearns <brandon.j.kearns@gmail.com>
2018-10-19 11:43:18 -0400

    Connects with contentful using dotenv to grabe spaceid and access token

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,24 @@
+const dotenv = require('dotenv');
+
+if (process.env.ENVIRONMENT !== 'production') {
+  dotenv.config();
+}
+
+const { spaceId, accessToken } = process.env;
+
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Connect Kicks',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-offline',
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId,
+        accessToken,
+      }
+    }
   ],
 }
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,8 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path')
 
-// You can delete this file if you're not using it
+exports.createPages = ({ graphql, boundActionCreators }) => {
+    const createPage = boundActionCreators
+    return new Promise((resolve, reject) => {
+        const kickShowcaseTemplate = path.resolve('src/components/')
+    })
+}
